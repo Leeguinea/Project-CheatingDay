75bafcc40736ef86d14083655583c592bd43caac
Leeguinea <129941496+Leeguinea@users.noreply.github.com>
2026-01-12 16:56:13 +0900

    Initial commit: Project setup and Map layout

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,26 @@
+using TMPro.EditorUtilities;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    float _speed = 10.0f;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.W))
+            transform.position += new Vector3(0.0f, 0.0f, 1.0f) * Time.deltaTime * _speed;
+        if (Input.GetKeyDown(KeyCode.S))
+            transform.position -= new Vector3(0.0f, 0.0f, 1.0f) * Time.deltaTime * _speed;
+        if (Input.GetKeyDown(KeyCode.A))
+            transform.position -= new Vector3(1.0f, 0.0f, 0.0f) * Time.deltaTime * _speed;
+        if (Input.GetKeyDown(KeyCode.D))
+            transform.position += new Vector3(1.0f, 0.0f, 0.0f) * Time.deltaTime * _speed;
+
+    }
+}
