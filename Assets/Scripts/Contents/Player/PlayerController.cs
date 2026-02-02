c7c878ae33f4081154c12ec72bc28e4e19069db3
Leeguinea <129941496+Leeguinea@users.noreply.github.com>
2026-02-03 06:48:14 +0900

    플레이어 음식 감지 로직 최적화 및 에디터 프리징 이슈 해결
    - Update 문을 Coroutine(0.1초 간격)으로 변경하여 연산 부하 감소
    
    - RaycastAll을 단일 Raycast로 변경하여 물리 연산 최적화
    
    - Unity 6 DX12 렌더링 충돌 문제를 DX11 전환으로 해결
    
    - 프레임 제한(Application.targetFrameRate = 60) 추가로 에디터 안정성 확보

--- Assets/Scripts/Contents/Player/PlayerController.cs
+++ Assets/Scripts/Contents/Player/PlayerController.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using TMPro.EditorUtilities;
 using UnityEngine;
 
@@ -17,15 +18,34 @@ public class PlayerController : MonoBehaviour
 
     CharacterController _controller;
 
+    bool _isLogSent = false;
+
     void Start()
     {
+        // 프레임을 60으로 고정합니다. (에디터 UI 먹통 방지)
+        Application.targetFrameRate = 60;
+
         _controller = GetComponent<CharacterController>();
 
         //중복 등록 방지
         Managers.Input.KeyAction -= OnKeyboard;
         Managers.Input.KeyAction += OnKeyboard;
+
+        StartCoroutine(CoCheckFoods());
     }
 
+    void Update()
+    {
+    }
+
+    IEnumerator CoCheckFoods()
+    {
+        while (true)
+        {
+            CheckFallinFoods();
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
 
     //InputManager가 매프레임마다 함수를 대신 호출해줌.
     void OnKeyboard()
@@ -54,4 +74,59 @@ public class PlayerController : MonoBehaviour
         _controller.Move((moveDir + gravity) * Time.deltaTime); //늘 중력을 받게 함.
 
     }
+
+    //[캐릭터 콜라이더]
+    void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        if(hit.gameObject.name == "Food")
+        {
+            Debug.Log($"충돌한 대상은 {hit.gameObject.name}이다.");
+        }        
+    }
+
+    //[트리거-아이템]
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.name == "Food")
+        {
+            Debug.Log($"{other.gameObject.name} 음식을 먹었다.");
+            Destroy(other.gameObject);
+        }
+    }
+
+    void CheckFallinFoods()
+    {
+        // 1. 레이저 발사 위치와 방향 설정
+        Vector3 rayStart = transform.position + Vector3.up * 1.5f;
+        Vector3 look = Vector3.up;
+        int mask = 1 << 8; // Food 레이어
+
+        
+        RaycastHit hit;
+        if (Physics.Raycast(rayStart, look, out hit, 10f, mask))
+        {
+            // 3. 중복 로그 방지 장치
+            if (!_isLogSent)
+            {
+                if (hit.collider.CompareTag("BadFood"))
+                {
+                    Debug.Log("Detected: BadFood");
+                }
+                else if (hit.collider.CompareTag("GoodFood"))
+                {
+                    Debug.Log("Detected: GoodFood");
+                }
+
+                _isLogSent = true;
+            }
+        }
+        else
+        {
+            // 4. 감지된 게 없으면 로그를 다시 보낼 수 있게 리셋
+            _isLogSent = false;
+        }
+    }
+
+
+
 }
