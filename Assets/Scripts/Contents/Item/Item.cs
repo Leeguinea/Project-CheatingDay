1aa140f039f241b628d55ded186f040902f52b47
Leeguinea <129941496+Leeguinea@users.noreply.github.com>
2026-03-08 22:13:35 +0900

    아이템 물리 충돌(바닥과 충돌 후 1초 대기 로직)
    1. 코루틴을 적용시켜 바닥(Ground)를 감지 후 1초 뒤 아이템이 비활성화가 됨.
    2. 아이템 프리팹의 RigidBody와 Capsule Collider에서 Is Trigger를 해제해야만 현상이 해결

--- Assets/Scripts/Contents/Item/Item.cs
+++ Assets/Scripts/Contents/Item/Item.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System.Collections;
 
 public class Item : MonoBehaviour
 {
@@ -10,27 +11,55 @@ public class Item : MonoBehaviour
 
     public enum ItemType { Target, Avoid }
     public ItemType type;
+    
+    private bool _isGrounded = false;
+
 
     //땅과 Item 충돌했을 때 비활성화
     private void OnCollisionEnter(Collision collision)
     {
-        if(collision.gameObject.CompareTag("Ground"))
+        if(!_isGrounded && collision.gameObject.CompareTag("Ground"))
         {
-            gameObject.SetActive(false);
+            _isGrounded = true;
+            StartCoroutine(DeactivateAfterDelay());
         }
     }
+
+    IEnumerator DeactivateAfterDelay()
+    {
+        yield return new WaitForSeconds(1.0f);
+
+        gameObject.SetActive(false);
+        _isGrounded = false;
+    }
+
 
     //플레이어가 Item을 먹었을 때 효과음 재생
     private void OnTriggerEnter(Collider other)
     {
-        AudioClip clipToPlay = (type == ItemType.Target) ? _targetSound : _AvoidSound;
-
+        Debug.Log("트리거 감지됨: " + other.gameObject.name);
         if (other.CompareTag("Player"))
         {
-            Debug.Log("플레이어 충돌 감지!"); 
-            SoundManager.Instance.PlaySFX(clipToPlay);
-        }
+            AudioClip clipToPlay = (type == ItemType.Target) ? _targetSound : _AvoidSound;
+            Debug.Log("플레이어 충돌 감지!");
 
-        gameObject.SetActive(false);
+            if(SoundManager.Instance != null && clipToPlay != null)
+            {
+                SoundManager.Instance.PlaySFX(clipToPlay);
+            }
+            else
+                Debug.Log("플레이어는 아니지만 트리거가 발동됨");
+
+            _isGrounded = false;
+            gameObject.SetActive(false);
+
+            
+        }
     }
+
+    private void OnEnable()
+    {
+        _isGrounded = false;
+    }
+
 }
